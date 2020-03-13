a9db7ccdf4bb64a25a4b395e6076179483db9d77
pathas <pathas@naver.com>
2020-03-13 15:02:51 +0900

    Add Sequelize, setting DB

---
+++ server/models/index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const path = require("path");
+const Sequelize = require("sequelize");
+
+const env = process.env.NODE_ENV || "development";
+const config = require(path.join(__dirname, "..", "config", "db.json"))[env];
+const db = {};
+
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config,
+  {
+    define: {
+      charset: "utf8",
+      collate: "utf8_general_ci"
+    }
+  }
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.log("Unable to connect to the database: ", err);
+  });
+
+// 생성한 테이블 연결
+db.Teacher = require("./teacher")(sequelize, Sequelize);
+
+db.Class = require("./class")(sequelize, Sequelize);
+
+// DB 관계 설정, foreignKey, Source - Target
+db.Teacher.hasOne(db.Class);
+
+db.secret = "(9*)5$&!3%^0%^@@2$1!#5@2!4";
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -4,16 +4,30 @@ const express = require("express");
 // app으로 서버를 관리할 수 있게 됨
 const app = express();
 
+// Sequeilize
+const sequelize = require("./models").sequelize;
+sequelize.sync();
+
+app.use(express.json());
+
 // PORT 번호 설정
 const PORT = process.env.PORT || 4000;
 
-// DB
-const db = require("./config/db");
+// 지정한 PORT 번호로 서버를 구동시킴, listen() 메소드 사용
+app.listen(PORT, () => {
+  console.log(`Server On: http://localhost:${PORT}/`);
+});
 
 // 라우터 설정(서버 응답 출력), get() 메소드
 app.get("/api/host", (req, res) => {
   res.send({ host: "pathas" });
 });
+
+/*
+MySQL 직접 연결
+
+// DB
+const db = require("./config/db");
 
 // DB 조회
 app.get("/api/test", (req, res) => {
@@ -26,8 +40,4 @@ app.get("/api/test", (req, res) => {
     }
   });
 });
-
-// 지정한 PORT 번호로 서버를 구동시킴, listen() 메소드 사용
-app.listen(PORT, () => {
-  console.log(`Server On: http://localhost:${PORT}/`);
-});
+*/
